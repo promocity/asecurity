9140d958865fa199b86db7177c848c8c28cb7a3b
Илья <verst.promocity.ekb@yandex.ru>
2015-10-28 13:57:16 +0500

    no message

---
+++ js/popup.js
@@ -0,0 +1,32 @@
+      var header_button = document.querySelector(".main-header-btn");
+      var footer_button = document.querySelector(".s-footer-callback");
+      var modal_window = document.querySelector(".modal-content-window");
+      var modal_background = document.querySelector(".modal-background");
+      var modal_content_close = modal_window.querySelector(".modal-content-close");
+
+      header_button.addEventListener("click", function(modal){
+        modal.preventDefault();
+        modal_window.classList.add("modal-content-show");
+        modal_background.classList.add("modal-content-show")
+      });
+
+
+      footer_button.addEventListener("click", function(modal){
+        modal.preventDefault();
+        modal_window.classList.add("modal-content-show");
+        modal_background.classList.add("modal-content-show")
+      });
+
+
+      modal_background.addEventListener("click", function(modal){
+        modal.preventDefault();
+        modal_window.classList.remove("modal-content-show");
+        modal_background.classList.remove("modal-content-show")
+      });
+
+
+      modal_content_close.addEventListener("click", function(modal){
+        modal.preventDefault();
+        modal_window.classList.remove("modal-content-show");
+        modal_background.classList.remove("modal-content-show")
+      });
